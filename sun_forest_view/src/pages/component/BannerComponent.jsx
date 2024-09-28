31f9ebb4c274136803ed8e64bfd96459f8e56525
green123456798 <sooyeon0119@naver.com>
2024-09-28 16:17:39 +0900

    사진 추가

--- sun_forest_view/src/pages/component/BannerComponent.jsx
+++ sun_forest_view/src/pages/component/BannerComponent.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import sunforestimg from "../../assets/img/sunforestimg.png"; 
+
 
 export default function banner() {
 
     return (
         <div style={{width: '100%', border: '2px solid black'}}>
-            <h1>banner</h1>
+    
+            <img style={{width:'100%', height: '300px'}} alt="bannerPicture" src={sunforestimg}></img>
         </div>
     );
 
