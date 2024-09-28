e037d44de33a9d286c066b44a34ad1dea308fc35
sooyeon <sooyeon0119@naver.com>
2024-09-28 13:12:07 +0900

    main 페이지 추가

---
+++ sun_forest_view/src/pages/Main.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+import Banner from "./component/Banner";
+
+export default function main() {
+    return (
+        <Banner />
+    );
+}
+
---
+++ sun_forest_view/src/pages/component/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function banner() {
+
+    return (
+        <div style={{width: 10000, border: '2px solid black'}}>
+            <h1>banner</h1>
+        </div>
+    );
+
+
+}
