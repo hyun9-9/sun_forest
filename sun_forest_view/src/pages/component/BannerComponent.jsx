ea69b0db1764fcfcb8c33f0b908f0af80240c2b6
DESKTOP-RNJN19C\sooye <sooyeon0119@naver.com>
2024-09-30 14:49:35 +0900

    배너 반응형 구현

--- sun_forest_view/src/pages/component/BannerComponent.jsx
+++ sun_forest_view/src/pages/component/BannerComponent.jsx
@@ -1,15 +1,47 @@
 import React from "react";
-import sunforestimg from "../../assets/img/sunforestimg.png"; 
+import squirrellogo from "../../assets/img/squirrellogo.png"; 
+import { useMediaQuery } from 'react-responsive';
 
-
-export default function banner() {
+export default function Banner() {
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+    const isTablet = useMediaQuery({ query: '(min-width: 769px) and (max-width: 1024px)' });
+    const isDesktop = useMediaQuery({ query: '(min-width: 1025px)' });
 
     return (
-        <div style={{width: '100%', border: '2px solid black'}}>
-    
-            <img style={{width:'100%', height: '300px'}} alt="bannerPicture" src={sunforestimg}></img>
+        <div>
+            {isDesktop && (
+                <>
+                <img
+                    alt="bannerPicture"
+                    src={squirrellogo}
+                />
+                <p>
+                    데스크탑
+                </p>
+                </>
+            )}
+
+            {isTablet && (
+                <>
+                    <img
+                        style={{ width: '800px' }} 
+                        alt="bannerPicture"
+                        src={squirrellogo}
+                    />
+                    <p>테블릿</p>
+                </>
+            )}
+
+            {isMobile && (
+                <>
+                    <img
+                        style={{ width: '360px' }} 
+                        alt="bannerPicture"
+                        src={squirrellogo}
+                    />
+                    <p>모바일</p>
+                </>
+            )}
         </div>
     );
-
-
 }
