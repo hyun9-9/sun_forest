f5cae05778f9b41523f193622cffb220e4b4eb07
HYUN9\pwcso <pwc1088@naver.com>
2024-10-09 16:18:30 +0900

    네비바

--- sun_forest_view/src/Router.jsx
+++ sun_forest_view/src/Router.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MyPage from "./pages/MyPage";
 import MainPage from "./pages/MainPage";
 import BannerComponent from "./pages/component/BannerComponent";
+import NavbarComponent from "./pages/component/NavbarComponent";
+import './App.css'; // 스타일 파일 추가
 
 export default function Router() {
-
   const memberId = 1;
-
-  console.log(memberId);
-
-  console.log('두개씩 나오는지');
 
   return (
     <BrowserRouter>
-      <BannerComponent memberId = {memberId}/>
-      <Routes>
-        <Route exact path='/' element={<MainPage />} />
-        <Route exact path='/my' element={<MyPage />} />
-      </Routes>
+    <BannerComponent memberId={memberId} />
+      <div className="layout">
+        <NavbarComponent memberId={memberId} />
+        <div className="content">
+          
+          <Routes>
+            <Route exact path='/' element={<MainPage />} />
+            <Route exact path='/my' element={<MyPage />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ sun_forest_view/src/pages/component/NavbarComponent.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import '../../assets/css/nav.css'
+
+export default function Navbar() {
+
+
+    return (
+    <>
+    <div class='nav'> 
+
+    </div>
+    <p> </p>
+    </>
+    );
+}
+
